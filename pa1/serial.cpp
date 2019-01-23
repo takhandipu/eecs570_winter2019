c739a822a1b41f242f5ffef572a5d84d9e6a5acc
Tanvir Ahmed Khan <takh@takh.eecs.umich.edu>
2019-01-23 14:44:46 -0500

    Timing functions are added to serial program

--- pa1/serial.cpp
+++ pa1/serial.cpp
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <sys/time.h>
+#include <stdint.h>
 
 #define DIM 1000
 
 long matrix_a[DIM][DIM];
 long matrix_b[DIM][DIM];
 long matrix_c[DIM][DIM];
+
+uint64_t get_current_time()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    uint64_t result = tv.tv_sec*(uint64_t)1000000+tv.tv_usec;
+    return result;
+} 
 
 void init()
 {
@@ -48,8 +58,18 @@ void print()
 
 int main(void)
 {
+    uint64_t start, end;
+    start=get_current_time();
     init();
+    end=get_current_time();
+    printf("Initialization time: %lu\n",end-start);
+    start=get_current_time();
     multiply();
+    end=get_current_time();
+    printf("Multiplication time: %lu\n",end-start);
+    start=get_current_time();
     print();
+    end=get_current_time();
+    printf("Print time: %lu\n",end-start);
     return 0;
 }
